2ce3f3eae42e3b850ba6809342e0280ccef7c835
Nandini13-rgb <65849848+Nandini13-rgb@users.noreply.github.com>
2020-07-02 19:06:49 +0530

    Update stock_price.py

--- stock_price.py
+++ stock_price.py
@@ -1,14 +1,20 @@
 def max_profit(arr):
+    max_profit = 0
     for i in range(len(arr)):
         buying_price = arr[i]
+
         if i == len(arr)-1:
-            return 0
+            return max_profit
         else:
             selling_price = max(arr[i + 1:len(arr)])
             if buying_price < selling_price:
                 profit = selling_price - buying_price
-                return profit
+                if profit > max_profit:
+                    max_profit = profit
             else:
                 continue
+
+
     return 0
-print(max_profit([]))
+
+print(max_profit([1,2,3,4]))
