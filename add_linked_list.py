73be750bd12257c1231033303ed8d42631e6a9c8
Nandini13-rgb <65849848+Nandini13-rgb@users.noreply.github.com>
2020-07-05 15:21:05 +0530

    Update add_linked_list.py

--- add_linked_list.py
+++ add_linked_list.py
@@ -2,9 +2,13 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+
 def insert(head, value):
     node = Node(value)
     if head is None:
@@ -15,6 +19,8 @@ def insert(head, value):
         temp = temp.next
     temp.next = node
     return head
+
+
 llist1 = LinkedList()
 llist1.head = insert(llist1.head, 5)
 llist1.head = insert(llist1.head, 4)
@@ -25,58 +31,68 @@ llist2 = LinkedList()
 llist2.head = insert(llist2.head, 3)
 llist2.head = insert(llist2.head, 6)
 llist2.head = insert(llist2.head, 9)
+
 def view(head):
     temp = head
     while temp:
         print(temp.data)
         temp = temp.next
 def add_list():
-    llist3 = LinkedList()
-    llist3.head = Node(0)
     temp1 = llist1.head
     temp2 = llist2.head
-    carry = 0
+    llist3 = LinkedList()
+    sum = temp1.data + temp2.data
+    if sum < 10:
+        node1 = Node(sum)
+        carry = 0
+    else:
+        node1 = Node(sum%10)
+        carry = sum//10
+    llist3.head = node1
+    result = node1
+    temp1 = temp1.next
+    temp2 = temp2.next
     while temp1 and temp2:
         sum = temp1.data + temp2.data
         if sum + carry < 10:
-            llist3.head.next = Node(sum + carry)
-            print(llist3.head.next.data)
+            # node1 = Node(sum)
+            result.next = Node(sum + carry)
+            result = result.next
         else:
-            llist3.head.next = Node((sum + carry) % 10)
-            print(llist3.head.next.data)
+            result.next = Node((sum + carry) % 10)
+            result = result.next
             carry = (sum + carry) // 10
         temp1 = temp1.next
         temp2 = temp2.next
-    while temp1 is not None: # if some element left in list1
+    while temp1 is not None:
         if carry > 0:
             if temp1.data+carry < 10:
-                llist3.head.next = Node(temp1.data+carry)
+                result.next = Node(temp1.data+carry)
                 carry = (temp1.data+carry)//10
-                print(llist3.head.next.data)
+                result = result.next
             else:
-                llist3.head.next = Node((temp1.data + carry)%10)
+                result.next = Node((temp1.data + carry)%10)
                 carry = (temp1.data + carry) // 10
-                print(llist3.head.next.data)
-
+                result = result.next
         else:
-            llist3.head.next = Node(temp1.data)
-            print(llist3.head.next.data)
+            result.next = Node(temp1.data)
+            result = result.next
         temp1 = temp1.next
-    while temp2 is not None:# if some element left in list2
+    while temp2 is not None:
         if carry > 0:
             if temp1.data+carry < 10:
-                llist3.head.next = Node(temp1.data+carry)
+                result.next = Node(temp1.data+carry)
                 carry = (temp1.data+carry)//10
-                print(llist3.head.next.data)
+                result = result.next
             else:
-                llist3.head.next = Node((temp1.data + carry)%10)
+                result.next = Node((temp1.data + carry)%10)
                 carry = (temp1.data + carry) // 10
-                print(llist3.head.next.data)
+                result = result.next
         else:
-            llist3.head.next = Node(temp1.data)
-            print(llist3.head.next.data)
+            result.next = Node(temp1.data)
+            result = result.next
         temp2 = temp2.next
-    if carry > 0: # if no element left in either list but carry lefts
-       llist3.head.next = Node(carry)
-       print(llist3.head.next.data)
+    if carry > 0:
+       result.next = Node(carry)
+    view(llist3.head)
 add_list()
